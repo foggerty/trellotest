261764615627e09ef4c99bd581f9578591ff7f6b
Matt Dee <matt.d.dee@gmail.com>
2017-02-14 17:51:51 +1300

    Now talks to Trello, but Trello doesn't like the auth tokens.

--- TrelloTest/Controllers/HomeController.cs
+++ TrelloTest/Controllers/HomeController.cs
@@ -1,27 +1,42 @@
 ﻿using System.Web.Mvc;
 using TrelloTest.Infrastructure.Logging;
+using TrelloTest.Infrastructure.TrelloClient;
+using TrelloTest.Models.Home;
 
 namespace TrelloTest.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILog _log;
+		private readonly ITrelloService _trelloService;
 
-		public HomeController(ILog log)
+		public HomeController(
+			ILog log,
+			ITrelloService trelloService)
 		{
 			_log = log;
+			_trelloService = trelloService;
 		}
 
 		public ActionResult Index()
 		{
 			// determine if user is authenticted against Trello.  If yes, redirect to boards page.
 
-			return View();
+			var url = _trelloService.Url;
+			var home = new Home
+			{
+				TrelloUrl = url.ToString(),
+				AccessToken = string.Empty
+			};
+
+			return View(home);
 		}
 
-		public RedirectToRouteResult Authenticate()
+		public RedirectToRouteResult Authenticate(Home home)
 		{
 			// if user is already authenticated, redirect to boards page
+
+			_trelloService.Authorise(home.AccessToken);
 
 			return RedirectToAction("boards", "cards");
 		}
---
+++ TrelloTest/Models/Home/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrelloTest.Models.Home
+{
+	public class Home
+	{
+		public string TrelloUrl { get; set; }
+
+		public string AccessToken { get; set; }
+	}
+}
